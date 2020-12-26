6e39d41ab8dddfad22d74d4ffa00ea6384651119
Michael Radionov <micrddrgn@gmail.com>
2020-12-26 15:33:13 +0300

    Cover missing slice header RPLR part

--- src/components/Payload/RefPicListReordering.vue
+++ src/components/Payload/RefPicListReordering.vue
@@ -50,7 +50,31 @@
         <Cell>{{ rplr.ref_pic_list_reordering_flag_l1 }}</Cell>
       </Row>
       <template v-if="rplr.ref_pic_list_reordering_flag_l1">
-        <TodoRow tag="rplr.ref_pic_list_reordering_flag_l1" />
+        <template v-for="(v, i) in reoder_l1_size">
+          <Row>
+            <Cell>rplr.reorder_l1.reordering_of_pic_nums_idc[{{ i }}]</Cell>
+            <Cell>{{ rplr.reorder_l1.reordering_of_pic_nums_idc[i] }}</Cell>
+          </Row>
+          <template
+            v-if="
+              rplr.reorder_l1.reordering_of_pic_nums_idc[i] === 0 ||
+                rplr.reorder_l1.reordering_of_pic_nums_idc[i] === 1
+            "
+          >
+            <Row>
+              <Cell>rplr.reorder_l1.abs_diff_pic_num_minus1[{{ i }}]</Cell>
+              <Cell>{{ rplr.reorder_l1.abs_diff_pic_num_minus1[i] }}</Cell>
+            </Row>
+          </template>
+          <template
+            v-else-if="rplr.reorder_l1.reordering_of_pic_nums_idc[i] === 2"
+          >
+            <Row>
+              <Cell>rplr.reorder_l1.long_term_pic_num[{{ i }}]</Cell>
+              <Cell>{{ rplr.reorder_l1.long_term_pic_num[i] }}</Cell>
+            </Row>
+          </template>
+        </template>
       </template>
     </template>
   </tbody>
@@ -90,6 +114,15 @@ export default {
     reorder_l0_size() {
       const size = this.getSize(
         this.rplr.reorder_l0.reordering_of_pic_nums_idc,
+      );
+      if (size > 0) {
+        return size + 1;
+      }
+      return 0;
+    },
+    reoder_l1_size() {
+      const size = this.getSize(
+        this.rplr.reorder_l1.reordering_of_pic_nums_idc,
       );
       if (size > 0) {
         return size + 1;
