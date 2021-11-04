93595fda19395ab1f73dc0dd93dce1fa65f32e20
Michael Radionov <micrddrgn@gmail.com>
2021-11-04 19:57:44 +1100

    Fix illegal invocation error

--- src/utils/threadUtils.js
+++ src/utils/threadUtils.js
@@ -2,19 +2,19 @@ const threadUtils = {
   // https://developers.google.com/web/updates/2015/08/using-requestidlecallback
   // Safari does not have it yet. Also used when runinng units tests because
   // Node does not have it either.
-  requestIdleCallback:
-    window.requestIdleCallback ||
-    function(cb) {
-      const start = Date.now();
-      return setTimeout(function() {
-        cb({
-          didTimeout: false,
-          timeRemaining: function() {
-            return Math.max(0, 50 - (Date.now() - start));
-          },
-        });
-      }, 1);
-    },
+  requestIdleCallback: window.requestIdleCallback
+    ? window.requestIdleCallback.bind(window)
+    : function(cb) {
+        const start = Date.now();
+        return setTimeout(function() {
+          cb({
+            didTimeout: false,
+            timeRemaining: function() {
+              return Math.max(0, 50 - (Date.now() - start));
+            },
+          });
+        }, 1);
+      },
 };
 
 export default threadUtils;
